42806ba096d4243866545bad59bad78f85d8bff3
Ivan Krakhmalyuk (LifeDJIK, lifedj27) <lifedj27@gmail.com>
2013-02-20 02:47:13 +0200

    Continue moving to cm-10.1

--- utab_7.6-vendor.mk
+++ utab_7.6-vendor.mk
@@ -1,8 +1,10 @@
-# LifeDJIK: NAND & MTD & similar modules.
-PRODUCT_COPY_FILES += \
-    vendor/targa/utab_7.6/modules/ehci-hcd.ko:root/lib/modules/ehci-hcd.ko \
-    vendor/targa/utab_7.6/modules/ohci-hcd.ko:root/lib/modules/ohci-hcd.ko \
-    vendor/targa/utab_7.6/modules/tcc_mtd.ko:root/lib/modules/tcc_mtd.ko \
-    vendor/targa/utab_7.6/modules/tcc_nand.ko:root/lib/modules/tcc_nand.ko \
-    vendor/targa/utab_7.6/modules/tcc_ndd.ko:root/lib/modules/tcc_ndd.ko \
-    vendor/targa/utab_7.6/modules/ufsd.ko:root/lib/modules/ufsd.ko
+# LifeDJIK: Copy vendor files.
+# LifeDJIK: TODO: use predefined variables (such as $(TARGET_RECOVERY_ROOT_OUT) & similar)
+
+# LifeDJIK: Find 'em all! ;-)
+vendor_files_root := $(strip $(wildcard vendor/targa/utab_7.6/root))
+vendor_files_system := $(strip $(wildcard vendor/targa/utab_7.6/system))
+
+# LifeDJIK: Copy 'em all! ;-)
+$(foreach item,$(vendor_files_root), cp -rf $(item) out/target/product/utab_7.6/root/)
+$(foreach item,$(vendor_files_system), cp -rf $(item) out/target/product/utab_7.6/system/)
