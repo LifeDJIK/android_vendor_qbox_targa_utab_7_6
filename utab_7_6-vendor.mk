e9ad061ddfcebf4e186751c4b0f728b0d053a331
Ivan Krakhmalyuk (LifeDJIK, lifedj27) <lifedj27@gmail.com>
2013-02-20 17:13:46 +0200

    Fixed name issue.

---
+++ utab_7_6-vendor.mk
@@ -0,0 +1,7 @@
+# LifeDJIK: Copy vendor files.
+PRODUCT_COPY_FILES += vendor/targa/utab_7_6/root/lib/modules/ehci-hcd.ko:root/lib/modules/ehci-hcd.ko
+PRODUCT_COPY_FILES += vendor/targa/utab_7_6/root/lib/modules/ohci-hcd.ko:root/lib/modules/ohci-hcd.ko
+PRODUCT_COPY_FILES += vendor/targa/utab_7_6/root/lib/modules/ufsd.ko:root/lib/modules/ufsd.ko
+PRODUCT_COPY_FILES += vendor/targa/utab_7_6/root/lib/modules/tcc_mtd.ko:root/lib/modules/tcc_mtd.ko
+PRODUCT_COPY_FILES += vendor/targa/utab_7_6/root/lib/modules/tcc_ndd.ko:root/lib/modules/tcc_ndd.ko
+PRODUCT_COPY_FILES += vendor/targa/utab_7_6/root/lib/modules/tcc_nand.ko:root/lib/modules/tcc_nand.ko
